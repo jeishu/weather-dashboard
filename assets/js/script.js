cbc03eda4446cdd02358deff9c14c2354fc344d0
jeishu <jeremy.zhu@utexas.edu>
2020-11-17 01:29:40 -0600

    Made design changes in CSS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,16 @@
-var apiKey = "8ca6b94fd6054266d22feab6d7957de4";
+// DOM Elements
+let searchCityEl = $("#search-city");
+    let searchVal = searchCityEl.val();
+
+let apiKey = "8ca6b94fd6054266d22feab6d7957de4";
+
+
+// Calling the AJAX functions
+
+$.ajax({
+    url: queryUrl,
+    method: "GET"
+  })
+  .then(function (response){
+
+  });
