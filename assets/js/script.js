113d5b110494381fa39cd4b64408a380f109e830
jeishu <jeremy.zhu@utexas.edu>
2020-11-17 18:03:09 -0600

    Changed the .text to .append to utilized the empty() to clear existing information for new searches.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,11 @@
 // DOM Elements
 let currentDayEl = $("#currentDay");
 
-let searchCityEl = $("#search-city").val().trim();
+let formEl = $("form");
+
+let customSec2El = $(".custom-sec2");
+
+// this is irrevelant now.. :(
 let cityEl = $(".city");
 let cityWrapperEl = $(".city-wrapper");
 let tempEl = $(".temp");
@@ -9,50 +13,108 @@ let humidityEl = $(".humidity");
 let windEl = $(".wind");
 let uviEl = $(".uvi");
 
-// The Current Time
-function currentTimeNow() {
-    let currentDisplay = moment().format("MMMM Do YYYY, h:mm a");
-    currentDayEl.html(currentDisplay);
-}
+let fiveDayEl = $("five-day-container");
 
-setInterval(currentTimeNow, 1000);
+let listGroupEl = $(".list-group");
 
+// Global Variables
 let apiKey = "8ca6b94fd6054266d22feab6d7957de4";
+let date = moment().format("L");
+
 
 // Calling the AJAX functions
-let queryURL = `http://api.openweathermap.org/data/2.5/weather?q=Killeen&units=imperial&appid=${apiKey}`;
+formEl.on("submit", function(event) {
+    event.preventDefault();
 
-let lat = "";
-let lon = "";
+    // Gets the value from the input text
+    let searchCityEl = $("#search-city").val().trim();
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-})
-.then(function (response){
-    console.log(queryURL);
-    console.log(response);
-    
-    cityEl.text(response.name);
-    
-    tempEl.text("Temperature: " + response.main.temp + "°F");
-    humidityEl.text("Humidity: " + response.main.humidity + "%");
-    windEl.text("Wind Speed: " + response.wind.speed + " MPH");
-    // Response to Weather, within the weather is an array, so index of 0 to call the icon number.
-    cityWrapperEl.append(`<img src="http://openweathermap.org/img/wn/${response.weather[0].icon}.png">`);
+    // Checks for a value
+    if(!searchCityEl){
+        return;
+    }
+    else{
+        console.log(searchCityEl);
+        let queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${searchCityEl}&units=imperial&appid=${apiKey}`;
+        // empties anything inside and gets everything ready to append new searches
+        customSec2El.empty();
 
-    let uviQueryURL = `http://api.openweathermap.org/data/2.5/uvi?appid=${apiKey}&lat=${response.coord.lat}&lon=${response.coord.lon}`;
+        // gets information for the current weather
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+        .then(function (response){
 
-    console.log(response.coord.lat);
-    console.log(response.coord.lon);
-    
-    $.ajax({
-    url: uviQueryURL,
-    method: "GET"
-    })
-    .then(function (response){  
-        uviEl.text("UV Index: " + response.value);
+            // checking for values
+            console.log(queryURL);
+            console.log(response);
+            
+            // Moved from .text to .append because it easier to empty() and .append then to change text, also template literals are cool
+            // city name and date
+            customSec2El.append(`<h3 style="font-size: 3rem">${response.name} ${date}</h3>`)
+            // Response to Weather, within the weather is an array, so index of 0 to call the icon number.
+            customSec2El.append(`<img src="http://openweathermap.org/img/wn/${response.weather[0].icon}.png">`);
+            // temperature
+            customSec2El.append(`<div class="temp weather-val">Temperature: ${response.main.temp}°F</div>`);
+            // humidity
+            customSec2El.append(`<div class="wind weather-val">Humidity: ${response.main.humidity}%</div>`);
+            // wind speed
+            customSec2El.append(`<div class="uvi weather-val">Wind Speed: ${response.wind.speed} MPH</div>`);
+            
+            // used to grab the longitude and latitude for the city for the UV Index
+            let uviQueryURL = `http://api.openweathermap.org/data/2.5/uvi?appid=${apiKey}&lat=${response.coord.lat}&lon=${response.coord.lon}`;
+
+            // Checking for coordinates
+            console.log(response.coord.lat);
+            console.log(response.coord.lon);
+            
+            // Getting UV INDEX
+            $.ajax({
+            url: uviQueryURL,
+            method: "GET"
+            })
+            .then(function (response){  
+                
+                // if the UV index is under 2, it will be green
+                if (response.value < 2) {
+                    customSec2El.append(`<div class="uvi weather-val low">UV Index: ${response.value}</div>`);
+                }
+                // if the UV index is between 2 and 5, it will be yellow
+                else if (response.value > 2 && response.value < 5){
+                    customSec2El.append(`<div class="uvi weather-val low-mid">UV Index: ${response.value}</div>`);
+                }
+                // if the UV index is between 5 and 8, it will be orange
+                else if (response.value > 5 && response.value < 8) {
+                    customSec2El.append(`<div class="uvi weather-val mid-high">UV Index: ${response.value}</div>`);
+                }
+                // if the UV index is over 8, it will be red
+                else {
+                    customSec2El.append(`<div class="uvi weather-val high">UV Index: ${response.value}</div>`);
+                }
+            });
+            
+            // grabbing the 5 day forecast from the searched city
+            let forecastQueryURL = `http://api.openweathermap.org/data/2.5/forecast?q=${searchCityEl}&appid=${apiKey}&units=imperial`;
+
+            // grabing info with AJAX
+            $.ajax({
+                url: forecastQueryURL,
+                method: "GET"
+            })
+            .then(function (response){  
+               for (let i = 0; i < 5; i++) {
+                    // getting the date    
+                   let displayDate = moment().add(i,"d").format("L");
+                   
+                   // Creating the forecast card
+                   let forecastCard = $("<div>")
+                }     
+
+
+
+            });
+                
     });
-    
+    }
 });
-
